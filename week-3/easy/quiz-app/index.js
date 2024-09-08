f9317157e2bc5e9d9dae476891c31138090c3ba2
Varun Dunna <varun.dunna2524@gmail.com>
2024-09-08 12:38:49 +0530

    8.9.2024

--- week-3/easy/quiz-app/index.js
+++ week-3/easy/quiz-app/index.js
@@ -2,6 +2,7 @@ let log = console.log;
 import { quizData } from "./data.js";
 log(quizData);
 let qNo = JSON.parse(sessionStorage.getItem("qNo"));
+log(qNo);
 // if (qNo > quizData.length) {
 //   alert("End of quiz.");
 //   // return;
@@ -17,107 +18,267 @@ if (qNo) {
       log("heereuh");
       let option = quizData[i].correct;
       let correctAns = quizData[i][option];
-      log(correctAns + " <-here");
+      // log(correctAns + " <-here");
       if (markedAnswers[i] == correctAns) {
         correctAnsCount++;
       }
     }
     document.body.innerHTML = `You have answered ${correctAnsCount}/${quizData.length} questions correct.`;
+    let backButton = document.createElement("button");
+    backButton.innerHTML = "Go back";
+    backButton.onclick = () => {
+      qNo--;
+      sessionStorage.setItem("qNo", qNo);
+    };
+    document.body.appendChild(backButton);
     // for(let i=0;i<)
     // document.body.innerText = "KEK Done";
     // document.body.appendChild("<p>Done !</p>");
   } else {
-    let entireDiv = document.createElement("div");
+    if (qNo >= 1) {
+      let entireDiv = document.createElement("div");
 
-    let question = document.createElement("h1");
+      let question = document.createElement("h1");
 
-    question.innerHTML = quizData[qNo].question;
-    let optionsDiv = document.createElement("div");
-    optionsDiv.setAttribute("class", `qNo-${qNo}`);
-    // for (let i = 0; i < quizData[qNo].length; i++) {
-    //   log(quizData[qNo].);
-    // }
-    // // let body = document.body;
+      question.innerHTML = quizData[qNo].question;
+      let optionsDiv = document.createElement("div");
+      optionsDiv.setAttribute("class", `qNo-${qNo}`);
+      // for (let i = 0; i < quizData[qNo].length; i++) {
+      //   log(quizData[qNo].);
+      // }
+      // // let body = document.body;
 
-    let option1 = document.createElement("input");
-    option1.setAttribute("type", "radio");
-    option1.setAttribute("name", "question");
+      let option1 = document.createElement("input");
+      option1.setAttribute("type", "radio");
+      option1.setAttribute("name", "question");
 
-    let label1 = document.createElement("label");
-    label1.innerHTML = quizData[qNo].a;
-    let option1Div = document.createElement("div");
-    option1Div.appendChild(option1);
-    option1Div.appendChild(label1);
-    // option1.appendChild(label1);
-    let option2 = document.createElement("input");
-    option2.setAttribute("type", "radio");
-    option2.setAttribute("name", "question");
-    let label2 = document.createElement("label");
-    label2.innerHTML = quizData[qNo].b;
-    // option2.appendChild(label2);
+      let label1 = document.createElement("label");
+      label1.innerHTML = quizData[qNo].a;
+      let option1Div = document.createElement("div");
+      option1Div.appendChild(option1);
+      option1Div.appendChild(label1);
+      // option1.appendChild(label1);
+      let option2 = document.createElement("input");
+      option2.setAttribute("type", "radio");
+      option2.setAttribute("name", "question");
+      let label2 = document.createElement("label");
+      label2.innerHTML = quizData[qNo].b;
+      // option2.appendChild(label2);
 
-    let option2Div = document.createElement("div");
-    option2Div.appendChild(option2);
-    option2Div.appendChild(label2);
-    let option3 = document.createElement("input");
-    option3.setAttribute("type", "radio");
-    option3.setAttribute("name", "question");
-    let label3 = document.createElement("label");
-    label3.innerHTML = quizData[qNo].c;
-    // option3.appendChild(label3);
+      let option2Div = document.createElement("div");
+      option2Div.appendChild(option2);
+      option2Div.appendChild(label2);
+      let option3 = document.createElement("input");
+      option3.setAttribute("type", "radio");
+      option3.setAttribute("name", "question");
+      let label3 = document.createElement("label");
+      label3.innerHTML = quizData[qNo].c;
+      // option3.appendChild(label3);
 
-    let option3Div = document.createElement("div");
-    option3Div.appendChild(option3);
-    option3Div.appendChild(label3);
-    let option4 = document.createElement("input");
-    option4.setAttribute("type", "radio");
-    option4.setAttribute("name", "question");
-    let label4 = document.createElement("label");
-    label4.innerHTML = quizData[qNo].d;
-    // option4.appendChild(label4);
+      let option3Div = document.createElement("div");
+      option3Div.appendChild(option3);
+      option3Div.appendChild(label3);
+      let option4 = document.createElement("input");
+      option4.setAttribute("type", "radio");
+      option4.setAttribute("name", "question");
+      let label4 = document.createElement("label");
+      label4.innerHTML = quizData[qNo].d;
+      // option4.appendChild(label4);
 
-    let option4Div = document.createElement("div");
-    option4Div.appendChild(option4);
-    option4Div.appendChild(label4);
-    optionsDiv.appendChild(option1Div);
-    optionsDiv.appendChild(option2Div);
-    optionsDiv.appendChild(option3Div);
-    optionsDiv.appendChild(option4Div);
-    let submit = document.createElement("button");
-    submit.setAttribute("type", "submit");
-    submit.innerHTML = "Submit";
-    submit.onclick = function () {
-      log(
-        document.querySelector("input[type=radio]:checked").parentElement
-          .children[1].innerHTML
-      );
+      let option4Div = document.createElement("div");
+      option4Div.appendChild(option4);
+      option4Div.appendChild(label4);
+      optionsDiv.appendChild(option1Div);
+      optionsDiv.appendChild(option2Div);
+      optionsDiv.appendChild(option3Div);
+      optionsDiv.appendChild(option4Div);
+      let submit = document.createElement("button");
+      submit.setAttribute("type", "submit");
+      submit.innerHTML = "Submit";
+      submit.onclick = function () {
+        let check = document.querySelector("input[type=radio]:checked");
+        if (!check) {
+          alert("Mark an option and then it can be submitted.");
+          return;
+        }
+        let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
+        if (markedAnswers) {
+          markedAnswers[qNo] = document.querySelector(
+            "input[type=radio]:checked"
+          ).parentElement.children[1].innerHTML;
+          sessionStorage.setItem(
+            "markedAnswers",
+            JSON.stringify(markedAnswers)
+          );
+        } else {
+          markedAnswers = {};
+          markedAnswers[qNo] = document.querySelector(
+            "input[type=radio]:checked"
+          ).parentElement.children[1].innerHTML;
+          sessionStorage.setItem(
+            "markedAnswers",
+            JSON.stringify(markedAnswers)
+          );
+        }
+        // sessionStorage prolly looks  like => []
+        // sessionStorage.setItem("markedAnswers",)
+        qNo++;
+        sessionStorage.setItem("qNo", qNo);
+
+        location.reload();
+      };
+      let backButton = document.createElement("button");
+      backButton.innerHTML = "Go back";
+      backButton.onclick = () => {
+        // history.back();
+        qNo--;
+        sessionStorage.setItem("qNo", qNo);
+        location.reload();
+      };
+      // document.body.appendChild(backButton);
+      entireDiv.appendChild(question);
+      entireDiv.appendChild(optionsDiv);
+      entireDiv.appendChild(submit);
+      entireDiv.appendChild(backButton);
+      document.body.appendChild(entireDiv);
       let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
-      if (markedAnswers) {
-        markedAnswers[qNo] = document.querySelector(
-          "input[type=radio]:checked"
-        ).parentElement.children[1].innerHTML;
-        sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
-      } else {
-        markedAnswers = {};
-        markedAnswers[qNo] = document.querySelector(
-          "input[type=radio]:checked"
-        ).parentElement.children[1].innerHTML;
-        sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
-      }
-      // sessionStorage prolly looks  like => []
-      // sessionStorage.setItem("markedAnswers",)
-      qNo++;
-      sessionStorage.setItem("qNo", qNo);
+      log(markedAnswers);
+      if (markedAnswers && markedAnswers[qNo]) {
+        for (let i = 0; i < optionsDiv.children.length; i++) {
+          // log(optionsDiv.children[i].children[1]);
 
-      location.reload();
-    };
-    entireDiv.appendChild(question);
-    entireDiv.appendChild(optionsDiv);
-    entireDiv.appendChild(submit);
-    document.body.appendChild(entireDiv);
+          if (
+            optionsDiv.children[i].children[1].innerHTML == markedAnswers[qNo]
+          ) {
+            optionsDiv.children[i].children[0].checked = true;
+          }
+        }
+      } else {
+      }
+    } else {
+      let entireDiv = document.createElement("div");
+
+      let question = document.createElement("h1");
+
+      question.innerHTML = quizData[qNo].question;
+      let optionsDiv = document.createElement("div");
+      optionsDiv.setAttribute("class", `qNo-${qNo}`);
+      // for (let i = 0; i < quizData[qNo].length; i++) {
+      //   log(quizData[qNo].);
+      // }
+      // // let body = document.body;
+
+      let option1 = document.createElement("input");
+      option1.setAttribute("type", "radio");
+      option1.setAttribute("name", "question");
+
+      let label1 = document.createElement("label");
+      label1.innerHTML = quizData[qNo].a;
+      let option1Div = document.createElement("div");
+      option1Div.appendChild(option1);
+      option1Div.appendChild(label1);
+      // option1.appendChild(label1);
+      let option2 = document.createElement("input");
+      option2.setAttribute("type", "radio");
+      option2.setAttribute("name", "question");
+      let label2 = document.createElement("label");
+      label2.innerHTML = quizData[qNo].b;
+      // option2.appendChild(label2);
+
+      let option2Div = document.createElement("div");
+      option2Div.appendChild(option2);
+      option2Div.appendChild(label2);
+      let option3 = document.createElement("input");
+      option3.setAttribute("type", "radio");
+      option3.setAttribute("name", "question");
+      let label3 = document.createElement("label");
+      label3.innerHTML = quizData[qNo].c;
+      // option3.appendChild(label3);
+
+      let option3Div = document.createElement("div");
+      option3Div.appendChild(option3);
+      option3Div.appendChild(label3);
+      let option4 = document.createElement("input");
+      option4.setAttribute("type", "radio");
+      option4.setAttribute("name", "question");
+      let label4 = document.createElement("label");
+      label4.innerHTML = quizData[qNo].d;
+      // option4.appendChild(label4);
+
+      let option4Div = document.createElement("div");
+      option4Div.appendChild(option4);
+      option4Div.appendChild(label4);
+      optionsDiv.appendChild(option1Div);
+      optionsDiv.appendChild(option2Div);
+      optionsDiv.appendChild(option3Div);
+      optionsDiv.appendChild(option4Div);
+      let submit = document.createElement("button");
+      submit.setAttribute("type", "submit");
+      submit.innerHTML = "Submit";
+      submit.onclick = function () {
+        log(
+          document.querySelector("input[type=radio]:checked").parentElement
+            .children[1].innerHTML
+        );
+        let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
+        if (markedAnswers) {
+          markedAnswers[qNo] = document.querySelector(
+            "input[type=radio]:checked"
+          ).parentElement.children[1].innerHTML;
+          sessionStorage.setItem(
+            "markedAnswers",
+            JSON.stringify(markedAnswers)
+          );
+        } else {
+          markedAnswers = {};
+          markedAnswers[qNo] = document.querySelector(
+            "input[type=radio]:checked"
+          ).parentElement.children[1].innerHTML;
+          sessionStorage.setItem(
+            "markedAnswers",
+            JSON.stringify(markedAnswers)
+          );
+        }
+        // sessionStorage prolly looks  like => []
+        // sessionStorage.setItem("markedAnswers",)
+        qNo++;
+        sessionStorage.setItem("qNo", qNo);
+
+        location.reload();
+      };
+      let backButton = document.createElement("button");
+      backButton.innerHTML = "Go back";
+      backButton.onclick = () => {
+        // history.back();
+        qNo--;
+        sessionStorage.setItem("qNo", qNo);
+        location.reload();
+      };
+      // document.body.appendChild(backButton);
+      entireDiv.appendChild(question);
+      entireDiv.appendChild(optionsDiv);
+      entireDiv.appendChild(submit);
+      entireDiv.appendChild(backButton);
+      document.body.appendChild(entireDiv);
+      let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
+      log(markedAnswers);
+      if (markedAnswers && markedAnswers[qNo]) {
+        for (let i = 0; i < optionsDiv.children.length; i++) {
+          // log(optionsDiv.children[i].children[1]);
+
+          if (
+            optionsDiv.children[i].children[1].innerHTML == markedAnswers[qNo]
+          ) {
+            optionsDiv.children[i].children[0].checked = true;
+          }
+        }
+      } else {
+      }
+    }
   }
 } else {
   qNo = 0;
+  log(qNo);
   // for(let i = 0;i<quizData)
   // log(quizData[qNo].a);
   let entireDiv = document.createElement("div");
@@ -180,23 +341,28 @@ if (qNo) {
   submit.setAttribute("type", "submit");
   submit.innerHTML = "Submit";
   submit.onclick = function () {
-    log(
-      document.querySelector("input[type=radio]:checked").parentElement
-        .children[1].innerHTML
-    );
+    let check = document.querySelector("input[type=radio]:checked");
+    if (!check) {
+      alert("Mark an option and then can be submitted.");
+      return;
+    }
     let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
+    log(markedAnswers + "hier");
     if (markedAnswers) {
       markedAnswers[qNo] = document.querySelector(
         "input[type=radio]:checked"
       ).parentElement.children[1].innerHTML;
       sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
     } else {
+      // markedAnswers = {};
       markedAnswers = {};
       markedAnswers[qNo] = document.querySelector(
         "input[type=radio]:checked"
       ).parentElement.children[1].innerHTML;
       sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
+      // sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
     }
+
     // sessionStorage prolly looks  like => []
     // sessionStorage.setItem("markedAnswers",)
     qNo++;
@@ -209,6 +375,37 @@ if (qNo) {
   entireDiv.appendChild(submit);
   document.body.appendChild(entireDiv);
   // // option1.innerHTML = quizData[qNo].a;
+
+  let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
+  log(markedAnswers);
+  // if (markedAnswers) {
+  //   markedAnswers[qNo] = document.querySelector(
+  //     "input[type=radio]:checked"
+  //   ).parentElement.children[1].innerHTML;
+  //   sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
+  // } else {
+  //   markedAnswers = {};
+  //   if (document.querySelector("input[type=radio]:checked")) {
+  //     // alert()
+
+  //     markedAnswers[qNo] = document.querySelector(
+  //       "input[type=radio]:checked"
+  //     ).parentElement.children[1].innerHTML;
+  //     sessionStorage.setItem("markedAnswers", JSON.stringify(markedAnswers));
+  //   }
+  // }
+  // let markedAnswers = JSON.parse(sessionStorage.getItem("markedAnswers"));
+  //     log(markedAnswers);
+  if (markedAnswers && markedAnswers[qNo]) {
+    for (let i = 0; i < optionsDiv.children.length; i++) {
+      // log(optionsDiv.children[i].children[1]);
+
+      if (optionsDiv.children[i].children[1].innerHTML == markedAnswers[qNo]) {
+        optionsDiv.children[i].children[0].checked = true;
+      }
+    }
+  } else {
+  }
 
   // option2.innerHTML = quizData[qNo].b;
   // option3.innerHTML = quizData[qNo].c;
