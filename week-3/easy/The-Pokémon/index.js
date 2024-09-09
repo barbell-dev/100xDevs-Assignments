9a5992b76fe2a52ccfc180e7050a5e4f1edae785
Varun Dunna <varun.dunna2524@gmail.com>
2024-09-09 23:05:09 +0530

    basic rendering

--- week-3/easy/The-Pokémon/index.js
+++ week-3/easy/The-Pokémon/index.js
@@ -11,6 +11,7 @@ let FindPokemons = document.createElement("button");
 FindPokemons.innerHTML = "Find Pokemons";
 
 FindPokemons.setAttribute("type", "submit");
+
 numberOfPokemons.setAttribute("type", "text");
 // numberOfPokemons.setAttribute("re")
 numberOfPokemons.required = true;
@@ -23,6 +24,36 @@ axios.get("https://pokeapi.co/api/v2/type/").then((response) => {
     categories.appendChild(option);
   }
   //   categories.
+});
+FindPokemons.addEventListener("click", function (event) {
+  let type = document.querySelector("select").value;
+  // log(type);
+  let noOfPokemons = document.querySelector("input[type=text]").value;
+  if (isNaN(noOfPokemons) || noOfPokemons == "") {
+    alert("Enter a valid number of pokemons.");
+    return;
+
+    // log("here");
+  } else {
+    // log("1");
+    let pokemonsOfCategory = document.createElement("div");
+    event.preventDefault();
+    axios.get(`https://pokeapi.co/api/v2/type/${type}`).then((response) => {
+      // location.reload();
+      for (let i = 0; i < noOfPokemons; i++) {
+        if (i == response.data.pokemon.length) {
+          break;
+        }
+        let pokemon = document.createElement("div");
+        pokemon.innerHTML = response.data.pokemon[i].pokemon.name;
+        // log(response.data.pokemon[i]);
+        pokemonsOfCategory.appendChild(pokemon);
+      }
+      // pokemonsOfCategory.length;
+      document.body.appendChild(pokemonsOfCategory);
+      // document.body.innerHTML = response.data.pokemon;
+    });
+  }
 });
 topDiv.appendChild(categories);
 topDiv.appendChild(numberOfPokemons);
