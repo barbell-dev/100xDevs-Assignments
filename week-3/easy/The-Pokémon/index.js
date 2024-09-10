0e46393414dd09ce3b438de2a25b0cea0c75c049
Varun Dunna <varun.dunna2524@gmail.com>
2024-09-10 13:04:31 +0530

    scrappy version done

--- week-3/easy/The-Pokémon/index.js
+++ week-3/easy/The-Pokémon/index.js
@@ -40,47 +40,84 @@ FindPokemons.addEventListener("click", function (event) {
     let allCardsDiv = document.createElement("div");
     allCardsDiv.style.display = "flex";
     event.preventDefault();
-    axios
-      .get(`https://pokeapi.co/api/v2/type/${type}`)
-      .then((response) => {
-        // location.reload();
-        for (let i = 0; i < noOfPokemons; i++) {
-          if (i == response.data.pokemon.length) {
-            break;
-          }
-          pokemonsOfCategory.push(response.data.pokemon[i].pokemon.name);
-          // let pokemon = document.createElement("div");
-          // pokemon.innerHTML = response.data.pokemon[i].pokemon.name;
-          // let card = document.createElement("div");
-          // card.setAttribute("class",`pokemon-${response.data.pokemon[i].pokemon.name}`);
-          // let img = document.createElement("img");
-          // // axios.get(`${response.data.pokemon[i].pokemon}`)
-
-          // // log(response.data.pokemon[i]);
-          // pokemonsOfCategory.appendChild(pokemon);
+    axios.get(`https://pokeapi.co/api/v2/type/${type}`).then((response) => {
+      // location.reload();
+      for (let i = 0; i < noOfPokemons; i++) {
+        if (i == response.data.pokemon.length) {
+          break;
         }
-        log(pokemonsOfCategory);
-        // pokemonsOfCategory.length;
-        // document.body.appendChild(pokemonsOfCategory);
-        // document.body.innerHTML = response.data.pokemon;
-      })
-      .then(() => {
-        for (let i = 0; i < pokemonsOfCategory.length; i++) {
-          let api = `https://pokeapi.co/api/v2/pokemon/${pokemonsOfCategory[i]}`;
-          // log(api);
-          axios.get(api).then((response) => {
-            log(response.data.sprites.front_default);
+        let arr = response.data.pokemon[i].pokemon.url.split("/");
+        log(arr);
+        // log(arr);
+        let id = arr[arr.length - 2];
+        pokemonsOfCategory.push(parseInt(id));
+        // let pokemon = document.createElement("div");
+        // pokemon.innerHTML = response.data.pokemon[i].pokemon.name;
+        // let card = document.createElement("div");
+        // card.setAttribute("class",`pokemon-${response.data.pokemon[i].pokemon.name}`);
+        // let img = document.createElement("img");
+        // // axios.get(`${response.data.pokemon[i].pokemon}`)
+
+        // // log(response.data.pokemon[i]);
+        // pokemonsOfCategory.appendChild(pokemon);
+      }
+      log(pokemonsOfCategory);
+      // pokemonsOfCategory.sort();
+      log(pokemonsOfCategory);
+      let promises = pokemonsOfCategory.map((id) => {
+        return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      });
+      Promise.all(promises)
+        .then((responses) => {
+          log(responses);
+          responses.forEach((response) => {
             let card = document.createElement("div");
-            card.setAttribute("class", `pokemon-${response.data.name}`);
+            card.setAttribute("class", `pokemon ${response.data.name}`);
             let img = document.createElement("img");
             img.src = response.data.sprites.front_default;
-            img.alt = pokemonsOfCategory[i];
+            img.alt = response.data.name + "'s image";
+            let caption = document.createElement("p");
+            caption.innerHTML = response.data.name;
             card.appendChild(img);
+            card.appendChild(caption);
+
             allCardsDiv.appendChild(card);
-            // document.body.appendChild()
           });
-        }
-      });
+          // pokemonsOfCategory.length;
+          // document.body.appendChild(pokemonsOfCategory);
+          // document.body.innerHTML = response.data.pokemon;
+        })
+        .catch(function (e) {
+          log(e);
+        });
+    });
+    // log(promises);
+
+    // .then(() => {
+    //   pokemonsOfCategory.sort();
+    //   log(pokemonsOfCategory);
+    //   for (let i = 0; i < pokemonsOfCategory.length; i++) {
+    //     log(pokemonsOfCategory[i]);
+    //     let api = `https://pokeapi.co/api/v2/pokemon/${pokemonsOfCategory[i]}`;
+    //     // log(api);
+    //     axios.get(api).then((response) => {
+    //       log(response.data.sprites.front_default);
+
+    //       let card = document.createElement("div");
+    //       card.setAttribute("class", `pokemon ${response.data.name}`);
+    //       let img = document.createElement("img");
+    //       img.src = response.data.sprites.front_default;
+    //       img.alt = pokemonsOfCategory[i];
+    //       let caption = document.createElement("p");
+    //       caption.innerHTML = response.data.name;
+    //       card.appendChild(img);
+    //       card.appendChild(caption);
+
+    //       allCardsDiv.appendChild(card);
+    //       // document.body.appendChild()
+    //     });
+    //   }
+    // });
     document.body.appendChild(allCardsDiv);
   }
 });
